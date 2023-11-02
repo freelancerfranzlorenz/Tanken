1567ae715aae04db7c42db6ac98dc05e0e269655
Franz Lorenz <freelancer_franzlorenz@web.de>
2023-11-02 11:49:28 +0100

    Optimze colors and delete empty lines

--- serviceworker.js
+++ serviceworker.js
@@ -16,16 +16,14 @@ const assetsToCache = [
 ];
 
 self.addEventListener( 'install', function( event ) 
-{
-   event.waitUntil( caches.open(CACHE_NAME)
+{   event.waitUntil( caches.open(CACHE_NAME)
       .then( function(cache) 
       {  return cache.addAll( assetsToCache ); } )
    );
 } );
 
 self.addEventListener('fetch', function(event) 
-{
-   event.respondWith( caches.match(event.request)
+{  event.respondWith( caches.match(event.request)
       .then( function(response) 
       {  return response || fetch(event.request); } )
    );
